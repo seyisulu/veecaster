e2d8e1c438e986c032dfa77822d038f916542a4b
Emmanuel Olowosulu <eolowo@unilag.edu.ng>
2016-05-16 10:47:19 +0100

    Fixed BinaryServer connection issues
    Signed-off-by: Emmanuel Olowosulu <eolowo@unilag.edu.ng>

--- client/components/seminars/livecs.js
+++ client/components/seminars/livecs.js
@@ -751,7 +751,7 @@ Template.livecs.events({
 
 Template.livecs.onRendered(function() {
   $('.menu .item').tab();
-  let lk = window.location, wk,
+  let lk = window.location, wk = lk.hostname,
       bh = Meteor.settings.public.veecaster.binsrvhost,
       bp = Meteor.settings.public.veecaster.binsrvport,
       bs = Meteor.settings.public.veecaster.binsrvpssl,
@@ -759,7 +759,7 @@ Template.livecs.onRendered(function() {
   if (lk.hostname === "localhost") {
     client = new BinaryClient(`ws://${bh}:${bl}`);
   } else {
-    client = new BinaryClient((lk.protocol==='http:') && `ws://${bh}:${bp}` || `wss://${bh}:${bs}`);
+    client = new BinaryClient((lk.protocol==='http:') && `ws://${wk}:${bp}` || `wss://${wk}:${bs}`);
   }
   if (FlowRouter.getParam('coach') === Meteor.userId()) {
     this.find('#uploading').style.display = 'none';
